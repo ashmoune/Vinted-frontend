62968337c8e6994075427834d478d94e29d5d502
ashmoune <146857243+ashmoune@users.noreply.github.com>
2024-05-06 13:08:58 +0200

    deploy

--- src/pages/Offer.jsx
+++ src/pages/Offer.jsx
@@ -14,7 +14,9 @@ const Offer = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`http://localhost:3000/offers/${id}`);
+        const response = await axios.get(
+          `https://site--backend-vinted--rh6mx4gc4kyd.code.run/offers/${id}`
+        );
         // console.log(response.data);
         SetData(response.data);
         SetIsLoading(false);
