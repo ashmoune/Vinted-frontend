07d0b7646272698f7f711f2fa18cdfe6d961c521
ashmoune <146857243+ashmoune@users.noreply.github.com>
2024-06-01 20:06:52 +0200

    footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -50,6 +50,14 @@ const Home = ({ search, sortPrice }) => {
           {/* composant card container */}
           <CardContainer data={data} />
         </div>
+        <footer>
+          <p>
+            {" "}
+            <span>{/* <img src="" alt="">  */}</span> Made at{" "}
+            <a href="https://www.lereacteur.io/"> Le Reacteur</a> by{" "}
+            <a href="https://github.com/ashmoune"> Hadrien MONNIOT</a>
+          </p>
+        </footer>{" "}
       </div>
     </>
   );
